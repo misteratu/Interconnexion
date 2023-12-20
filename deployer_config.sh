2e9cbf70ef6d895a657a336975cc5e0ddaac4ebe
Alexandre Trotel <trotelalexandre@proton.me>
2023-12-20 11:39:39 +0100

    deployer

--- deployer_config.sh
+++ deployer_config.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-# construction des images à partir des dockerfiles
-docker build ./dockerfiles/R1 -t image_routeur_r1
-docker build ./dockerfiles/R1EN -t image_routeur_r1en
-docker build ./dockerfiles/R2 -t image_routeur_r2
-docker build ./dockerfiles/R2EN -t image_routeur_r2en
-docker build ./dockerfiles/R3 -t image_routeur_r3
-docker build ./dockerfiles/R4 -t image_routeur_r4
-docker build ./dockerfiles/PRIVE/Machine1 -t image_prive_machine1 
-docker build ./dockerfiles/PRIVE/Machine2 -t image_prive_machine2
-docker build ./dockerfiles/BOX1 -t image_box1
-docker build ./dockerfiles/BOX2 -t image_box2
+# construction des Image/dockerfiless à partir des Image/dockerfiles
+docker build ./Image/dockerfiles/R1 -t Image/dockerfiles_routeur_r1
+docker build ./Image/dockerfiles/R1EN -t Image/dockerfiles_routeur_r1en
+docker build ./Image/dockerfiles/R2 -t Image/dockerfiles_routeur_r2
+docker build ./Image/dockerfiles/R2EN -t Image/dockerfiles_routeur_r2en
+docker build ./Image/dockerfiles/R3 -t Image/dockerfiles_routeur_r3
+docker build ./Image/dockerfiles/R4 -t Image/dockerfiles_routeur_r4
+docker build ./Image/dockerfiles/PRIVEE/Machine1 -t Image/dockerfiles_prive_machine1 
+docker build ./Image/dockerfiles/PRIVEE/Machine2 -t Image/dockerfiles_prive_machine2
+docker build ./Image/dockerfiles/BOX1 -t Image/dockerfiles_box1
+docker build ./Image/dockerfiles/BOX2 -t Image/dockerfiles_box2
 
