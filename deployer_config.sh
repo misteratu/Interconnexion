ce3bdd3656c10ece17aa415f751384725df12cde
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2023-12-20 11:06:47 +0100

    Architecture

--- deployer_config.sh
+++ deployer_config.sh
@@ -6,8 +6,8 @@ docker build ./dockerfiles/R2 -t image_routeur_r2
 docker build ./dockerfiles/R2EN -t image_routeur_r2en
 docker build ./dockerfiles/R3 -t image_routeur_r3
 docker build ./dockerfiles/R4 -t image_routeur_r4
-docker build ./dockerfiles/PRIVE_ENTREPRISE/SERVEUR -t image_serv_dhcp 
-docker build ./dockerfiles/PRIVE_ENTREPRISE/MACHINE1 -t image_machine_prive
+docker build ./dockerfiles/PRIVE/Machine1 -t image_prive_machine1 
+docker build ./dockerfiles/PRIVE/Machine2 -t image_prive_machine2
 docker build ./dockerfiles/BOX1 -t image_box1
 docker build ./dockerfiles/BOX2 -t image_box2
 
