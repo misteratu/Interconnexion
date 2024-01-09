d7ba57408cb37dea9a7953b38176fcb52fa1cfd3
Alexandre Trotel <trotelalexandre@proton.me>
2024-01-09 20:31:24 +0100

    fix

--- Image/deployer_config.sh
+++ Image/deployer_config.sh
@@ -10,11 +10,12 @@ docker build ./dockerfiles/R2 -t image_routeur_r2
 docker build ./dockerfiles/R2EN -t image_routeur_r2en
 docker build ./dockerfiles/R3 -t image_routeur_r3
 docker build ./dockerfiles/R4 -t image_routeur_r4
-docker build ../DockerFiles/DNS_n7 -t image_dns
+docker build ./dockerfiles/DNS_n7 -t image_dns
 
 docker run --cap-add=NET_ADMIN image_routeur_box1
 docker run --cap-add=NET_ADMIN image_routeur_box2
-docker run --cap-add=NET_ADMIN image_routeur_privee
+docker run --cap-add=NET_ADMIN image_routeur_privee_machine1
+docker run --cap-add=NET_ADMIN image_routeur_privee_machine2
 docker run --cap-add=NET_ADMIN image_routeur_r1
 docker run --cap-add=NET_ADMIN image_routeur_r1en
 docker run --cap-add=NET_ADMIN image_routeur_r2
