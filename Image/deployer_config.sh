0dba743599510005ac2002a410f20d35efac0197
Alexandre Trotel <trotelalexandre@proton.me>
2024-01-10 12:44:35 +0100

    fix

--- Image/deployer_config.sh
+++ Image/deployer_config.sh
@@ -18,25 +18,38 @@ docker network create --subnet=120.0.40.0/22 --driver=bridge ReseauEntreprise
 docker network create --subnet=120.0.32.0/21 --driver=bridge ReseauClient
 docker network create --subnet=120.0.44.0/23 --driver=bridge ReseauService
 
-docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_box1 image_routeur_box1 --network ReseauClient
-#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_box2 image_routeur_box2 --network ReseauClient
-#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_privee_machine1 image_routeur_privee_machine1 --network ReseauClient
-#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_privee_machine2 image_routeur_privee_machine2 --network ReseauClient
-docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r1 image_routeur_r1 --network ReseauBackBone
-docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r1en image_routeur_r1en --network ReseauEntreprise
-docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r2 image_routeur_r2 --network ReseauBackBone
-#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r2en image_routeur_r2en --network ReseauEntreprise
-docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r3 image_routeur_r3 --network ReseauClient
-docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r4 image_routeur_r4 --network ReseauBackBone
-docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_dns image_dns --network ReseauService
+docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_box1 image_routeur_box1
+#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_box2 image_routeur_box2
+#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_privee_machine1 image_routeur_privee_machine1
+#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_privee_machine2 image_routeur_privee_machine2
+docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r1 image_routeur_r1
+docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r1en image_routeur_r1en
+docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r2 image_routeur_r2
+#docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r2en image_routeur_r2en
+docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r3 image_routeur_r3
+docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_r4 image_routeur_r4
+docker run -d --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=SYS_ADMIN --name container_dns image_dns
+
+# Connexion aux réseaux 
+#docker network connect ReseauClient container_box1
+#docker network connect ReseauClient container_box2
+#docker network connect ReseauClient container_privee_machine1
+#docker network connect ReseauClient container_privee_machine2
+#docker network connect ReseauBackBone container_r1
+#docker network connect ReseauEntreprise container_r1en
+#docker network connect ReseauBackBone container_r2
+#docker network connect ReseauEntreprise container_r2en
+#docker network connect ReseauClient container_r3
+#docker network connect ReseauBackBone container_r4
+#docker network connect ReseauService container_dns
 
 # Création interfaces
-docker network connect --ip 120.0.32.2 --alias eth0 ReseauClient container_box1
-docker network connect --ip 120.0.46.1 --alias eth0 ReseauClient container_r1
-docker network connect --ip 120.0.40.3 --alias eth0 ReseauClient container_r1en
-docker network connect --ip 120.0.46.2 --alias eth0 ReseauClient container_r2
-docker network connect --ip 120.0.40.1 --alias eth1 ReseauClient container_r2
-docker network connect --ip 120.0.46.3 --alias eth0 ReseauClient container_r3
-docker network connect --ip 120.0.32.1 --alias eth1 ReseauClient container_r3
-docker network connect --ip 120.0.46.4 --alias eth0 ReseauClient container_r4
-docker network connect --ip 120.0.40.1 --alias eth1 ReseauClient container_r4
+docker network connect --ip 120.0.32.2/21 --alias eth0 ReseauClient container_box1
+docker network connect --ip 120.0.46.1/23 --alias eth0 ReseauBackBone container_r1
+docker network connect --ip 120.0.40.3/22 --alias eth0 ReseauEntreprise container_r1en
+docker network connect --ip 120.0.46.2/23 --alias eth0 ReseauBackBone container_r2
+docker network connect --ip 120.0.40.1/22 --alias eth1 ReseauBackBone container_r2
+docker network connect --ip 120.0.46.3/23 --alias eth0 ReseauBackBone container_r3
+docker network connect --ip 120.0.32.1/21 --alias eth1 ReseauBackBone container_r3
+docker network connect --ip 120.0.46.4/23 --alias eth0 ReseauBackBone container_r4
+docker network connect --ip 120.0.40.1/22 --alias eth1 ReseauBackBone container_r4
