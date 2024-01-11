2f6fb1f293afda31d7c01c1283eba79b0a121edb
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2024-01-11 16:07:04 +0100

    ⚓️

--- Image/dockerfiles/BOX1/script.sh
+++ Image/dockerfiles/BOX1/script.sh
@@ -6,18 +6,33 @@
 ip link add eth1 type veth peer name eth2 type veth
 
 ip link set dev eth1 up
-#ip addr add flush dev eth0
+ip link set dev eth2 up
 
 # adresses
 
 ip addr add 120.0.32.2/21 dev eth1
 
+# Activation du routage
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Configuration DHCP
+service isc-dhcp-server start
+
 # routes
 
-#ip route add 120.0.40.0/22 via 120.0.46.4
-#ip route add 120.0.44.0/23 via 120.0.46.2
-#ip route add 120.0.32.0/21 via 120.0.46.3
+ip r a default via 120.0.32.1
 
+# NAT
+iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp -i eth1 --dport 80 -j DNAT --to 192.168.139.129
+
+
+# FIREWALL
+iptables -A FORWARD -i eth1 -j DROP
+iptables -A FORWARD -i eth1 -p ICMP -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -dport 80 -j ACCEPT
+iptables -A FORWARD -i eth1 -p tcp -dport 53 -j ACCEPT
+iptables -A FORWARD -i eth1 -p udp -dport 53 -j ACCEPT
 # quagga
 
 /etc/init.d/zebra start
--- Image/dockerfiles/BOX1/start.sh
+++ Image/dockerfiles/BOX1/start.sh
@@ -4,10 +4,6 @@
 ip addr add 120.0.32.2/21 dev eth0
 ip link set dev eth0 up
 
-# Interface serveur privé
-ip addr add 192.168.1.1/24 dev eth1
-ip link set dev eth1 up
-
 # Activation du routage
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
